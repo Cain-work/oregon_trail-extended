63ba22c9f6d3fceb44b50d18a4fcd57dc7992982
Tony Cain <r.tonycain@gmail.com>
2020-07-07 16:22:44 -0400

    inital commit

---
+++ main.js
@@ -0,0 +1,34 @@
+class traveler {
+    constructor(name, food, isHealthy) {
+        this.name = name
+        this.food = 1
+        this.isHealthy = true
+    }
+    hunt() {
+        return food = ++2
+    }
+    eat() {
+        return food = - 1
+        if (food >= 0, this.isHealthy = false);
+    }
+}
+
+class wagon {
+    constructor(capacity, passengers){
+        this.capacity = 6
+        this.passengers = name[]
+    }
+    getAvailableSeatCount(){
+        return emptySeats(6 >= passengers.legnth)
+    }
+    join(traveler){
+
+    }
+    shouldQuarantion(){
+
+    }
+    totalFood(){
+        
+    }
+}
+
